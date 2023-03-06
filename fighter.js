9bcc1e1b94e95a0fae206e36641cc62cfbea3530
Igorson01 <josefkrolewski@gmail.com>
2023-03-07 00:38:34 +0100

    fighter.js

--- fighter.js
+++ fighter.js
@@ -222,7 +222,7 @@ function animate() {
          player.isAttacking && player.framesCurrent === 4
          ) {
          player.isAttacking = false
-         enemy.health -= 15
+         enemy.health -= 20
          document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
     if(player.isAttacking && player.framesCurrent === 4) {
